3f5bf79e9c3d7875923b6b579bac27190cd847ca
Jim Conner <jamesdavidconner@gamil.com>
2021-01-26 20:43:38 -0600

    forEach example

---
+++ main.js
@@ -0,0 +1,11 @@
+// .forEach
+
+const array = [1,2,3,4,5,6];
+
+array.forEach((item, i) => {
+  console.log(item, i);
+})
+
+// for (let i = 0; i < Array.length; i++){
+//   console.log(array[i])
+// }
